18203ad889775fe7bdc4861a107af09a2cd5f164
visual-pivert <visualpivert@gmail.com>
2025-11-09 08:29:40 +0300

    adding fish

---
+++ fish/config.fish
@@ -0,0 +1,24 @@
+# Enable syntax highlighting and autosuggestions
+set -g fish_color_command green
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection magenta
+set -g fish_color_error red
+
+# Fuzzy finder keybinds (requires fzf.fish)
+fzf_configure_bindings --history --directory
+
+# Aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Better greeting
+set -g fish_greeting "🐠 Welcome back, $(whoami)!  Dive in."
+
+# Path tweaks (if needed)
+set -Ux PATH $PATH ~/.local/bin
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
